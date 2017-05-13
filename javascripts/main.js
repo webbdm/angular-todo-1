648c77d0f77f2bbae612d375cd52f97dc84b98bf
Zoe Ames <zoeames@gmail.com>
2017-05-13 10:57:40 -0500

    firebase setup

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,3 @@
-var app = angular.module("TodoApp", []);
-
-
 app.controller("NavCtrl", ($scope) => {
 	$scope.cat = "Meow";
 	$scope.navItems= [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
@@ -10,26 +7,7 @@ app.controller("NavCtrl", ($scope) => {
 app.controller("ItemCtrl", ($scope) => {
 	$scope.dog = "Woof!";
 	$scope.showListView = true;
-  $scope.items = [
-    {
-      id: 0,
-      task: "mow the lawn",
-      isCompleted: true,
-      assignedTo: "Callan",
-    },
-    {
-      id: 1,
-      task: "grade quizzes",
-      isCompleted: false,
-      assignedTo: "Lauren",
-    },
-    {
-      id: 2,
-      task: "take a nap",
-      isCompleted: false,
-      assignedTo: "Zoe",
-    }
-  ];
+  $scope.items = [];
 
 	$scope.newItem = () => {
 		$scope.showListView = false;
