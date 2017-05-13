00c5c0e7811f3ab6b6900b11d10f234174b6a042
Zoe Ames <zoeames@gmail.com>
2017-05-13 11:42:45 -0500

    GET working

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,14 @@
+app.run((FIREBASE_CONFIG) => {
+	firebase.initializeApp(FIREBASE_CONFIG);
+});
+
 app.controller("NavCtrl", ($scope) => {
 	$scope.cat = "Meow";
 	$scope.navItems= [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
 });
 
 
-app.controller("ItemCtrl", ($scope) => {
+app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
 	$scope.dog = "Woof!";
 	$scope.showListView = true;
   $scope.items = [];
@@ -16,6 +20,39 @@ app.controller("ItemCtrl", ($scope) => {
 	$scope.allItems = () => {
 		$scope.showListView = true;
 	};
+
+	let getItemList = () => {
+		let itemz = [];
+		return $q((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
+				.then((fbItems) => {
+					let itemCollection = fbItems.data;
+          Object.keys(itemCollection).forEach((key) => {
+            itemCollection[key].id=key;
+            itemz.push(itemCollection[key]);
+          });
+          resolve(itemz);
+				})
+				.catch((error) => {
+					reject(error);
+				})
+		});
+	};
+
+	let getItems = () => {
+		getItemList().then((itemz) => {
+			$scope.items = itemz;
+		}).catch((error) => {
+			console.log("get Error", error)
+		});
+	};
+
+	getItems();
+
+
+	
+
+
 
 
 });
