3219ed309afe6e9fdad48f45877fff383358baa2
Zoe Ames <zoeames@gmail.com>
2017-05-15 21:16:30 -0500

    removed main.js

--- javascripts/controllers/ItemNewCtrl.js
+++ javascripts/controllers/ItemNewCtrl.js
@@ -1,3 +1,22 @@
-app.controller("ItemNewCtrl", function() {
-	console.log("inside ItemNewCtrl");
+app.controller("ItemNewCtrl", function($http, $q, $scope, FIREBASE_CONFIG) {
+	let postNewItem = (newItem) => {
+		return $q((resolve, reject) =>{
+			$http.post(`${FIREBASE_CONFIG.databaseURL}/items.json`, JSON.stringify(newItem))
+				.then((resultz) => {
+					resolve(resultz);
+				}).catch((error) => {
+					reject(error);
+				});
+		});
+	};
+
+	$scope.addNewItem = () => {
+		$scope.newTask.isCompleted = false;
+		postNewItem($scope.newTask).then(() => {
+			$scope.newTask = {};
+			// switch views
+		}).catch((error) => {
+			console.log("Add error", error);
+		});
+	};
 });
