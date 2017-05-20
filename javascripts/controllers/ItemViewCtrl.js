6e3301ce55ae004be01443bbe78796802715cd4b
Zoe Ames <zoeames@gmail.com>
2017-05-20 10:14:58 -0500

    done

--- javascripts/controllers/ItemViewCtrl.js
+++ javascripts/controllers/ItemViewCtrl.js
@@ -1,3 +1,12 @@
-app.controller("ItemViewCtrl", function() {
-	console.log("inside ItemViewCtrl");
+app.controller("ItemViewCtrl", function($routeParams, $scope, ItemFactory) {
+	$scope.selectedItem = {};
+
+	ItemFactory.getSingleItem($routeParams.id).then((results) => {
+		console.log("results", results);
+		$scope.selectedItem = results.data;
+	}).catch((error) => {
+		console.log("getSingleItem", error);
+	});
+
+
 });
--- javascripts/factories/ItemFactory.js
+++ javascripts/factories/ItemFactory.js
@@ -19,6 +19,18 @@ app.factory("ItemFactory", function($q, $http, FIREBASE_CONFIG){
 		});
 	};
 
+	let getSingleItem = (id) => {
+		return $q((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/items/${id}.json`)
+			.then((resultz) => {
+				resultz.data.id =id;
+				resolve(resultz);
+			}).catch((error) => {
+				reject(error);
+			});
+		});
+	};
+
 	let postNewItem = (newItem) => {
 		return $q((resolve, reject) =>{
 			$http.post(`${FIREBASE_CONFIG.databaseURL}/items.json`, JSON.stringify(newItem))
@@ -57,7 +69,7 @@ app.factory("ItemFactory", function($q, $http, FIREBASE_CONFIG){
 		});
 	};
 
-	return {getItemList:getItemList, postNewItem:postNewItem, deletz:deletz, editItem:editItem};
+	return {getItemList:getItemList, getSingleItem:getSingleItem, postNewItem:postNewItem, deletz:deletz, editItem:editItem};
 
 
 });
