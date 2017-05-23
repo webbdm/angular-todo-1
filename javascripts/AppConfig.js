f771dc532c7ee456c46c27d35e9a511d0c8401a3
webbdm <gwebb762@gmail.com>
2017-05-22 20:11:19 -0500

    Added login/logout

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -1,24 +1,32 @@
 app.run(function(FIREBASE_CONFIG) {
-	firebase.initializeApp(FIREBASE_CONFIG);
+    firebase.initializeApp(FIREBASE_CONFIG);
 });
 
 app.config(function($routeProvider) {
-	$routeProvider
-		.when('/items/list', {
-			templateUrl: 'partials/item-list.html',
-			controller: 'ItemListCtrl'
-		})
-		.when('/items/new', {
-			templateUrl: 'partials/item-new.html',
-			controller: 'ItemNewCtrl'
-		})
-		.when('/item/view/:id', {
-			templateUrl: 'partials/item-view.html',
-			controller: 'ItemViewCtrl'
-		})
-		.when('/item/edit/:id', {
-			templateUrl: 'partials/item-new.html',
-			controller: 'ItemEditCtrl'
-		})
-		.otherwise('/items/list');
+    $routeProvider
+        .when('/auth', {
+            templateUrl: 'partials/auth.html',
+            controller: 'AuthCtrl'
+        })
+        .when('/items/list', {
+            templateUrl: 'partials/item-list.html',
+            controller: 'ItemListCtrl'
+        })
+        .when('/items/new', {
+            templateUrl: 'partials/item-new.html',
+            controller: 'ItemNewCtrl'
+        })
+        .when('/item/view/:id', {
+            templateUrl: 'partials/item-view.html',
+            controller: 'ItemViewCtrl'
+        })
+        .when('/item/edit/:id', {
+            templateUrl: 'partials/item-new.html',
+            controller: 'ItemEditCtrl'
+        })
+        .when('/logout', {
+            templateUrl: 'partials/auth.html',
+            controller: 'AuthCtrl'
+        })
+        .otherwise('/items/auth');
 });
