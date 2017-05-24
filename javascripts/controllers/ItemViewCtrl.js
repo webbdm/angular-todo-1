dade609525caecca30c3d7125547922913bf3821
webbdm <gwebb762@gmail.com>
2017-05-23 21:42:42 -0500

    Added tool

--- javascripts/controllers/ItemViewCtrl.js
+++ javascripts/controllers/ItemViewCtrl.js
@@ -1,12 +1,18 @@
-app.controller("ItemViewCtrl", function($routeParams, $scope, ItemFactory) {
-	$scope.selectedItem = {};
+app.controller("ItemViewCtrl", function($routeParams, $scope, ItemFactory, ToolFactory) {
+    $scope.selectedItem = {};
 
-	ItemFactory.getSingleItem($routeParams.id).then((results) => {
-		console.log("results", results);
-		$scope.selectedItem = results.data;
-	}).catch((error) => {
-		console.log("getSingleItem", error);
-	});
+    ItemFactory.getSingleItem($routeParams.id).then((results) => {
+        console.log("results", results);
+        $scope.selectedItem = results.data;
+    }).catch((error) => {
+        console.log("getSingleItem", error);
+    });
+
+    ToolFactory.getToolList($routeParams.id).then(() => {
+    	$scope.tools = results;
+    }).catch((error) => {
+    	console.log("error in getToolList", error);
+    });
 
 
 });
