4e6b2895e6e34b6117cb42f1b71588ac72d1bd37
webbdm <gwebb762@gmail.com>
2017-05-23 19:37:50 -0500

    Auth

--- javascripts/controllers/ItemNewCtrl.js
+++ javascripts/controllers/ItemNewCtrl.js
@@ -1,13 +1,13 @@
-app.controller("ItemNewCtrl", function($http, $location, $q, $scope, FIREBASE_CONFIG, ItemFactory) {
+app.controller("ItemNewCtrl", function($rootScope, $http, $location, $q, $scope, FIREBASE_CONFIG, ItemFactory) {
 
-	$scope.addNewItem = () => {
-		$scope.newTask.isCompleted = false;
-		$scope.newTask.uid = $rootScope.user.uid;
-		ItemFactory.postNewItem($scope.newTask).then(() => {
-			$scope.newTask = {};
-			$location.url("/items/list");
-		}).catch((error) => {
-			console.log("Add error", error);
-		});
-	};
+    $scope.addNewItem = () => {
+        $scope.newTask.isCompleted = false;
+        $scope.newTask.uid = $rootScope.user.uid;
+        ItemFactory.postNewItem($scope.newTask).then(() => {
+            $scope.newTask = {};
+            $location.url("/items/list");
+        }).catch((error) => {
+            console.log("Add error", error);
+        });
+    };
 });
